4ac1a9d9b42309099dd6d38be056ef8ac1a02f01
Erick Ueda <zarebu50@gmail.com>
2026-09-24 10:25:37 -0300

    Player e Movement

---
+++ My project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float speed;
+
+    private Rigidbody rb;
+    private Vector3 movement;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        movement = new Vector3(horizontal, 0f, vertical).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
